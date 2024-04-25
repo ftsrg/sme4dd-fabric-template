cda8a3c95a41ae72c079ef864dd4377ae3c9c9c0
bp99 <bertalan.peter@bp99.eu>
2024-04-25 23:59:18 +0200

    Add Java skeleton project

---
+++ components/thesis-portal-chaincode-java/build.gradle.kts
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+  application
+  id("com.github.johnrengelman.shadow") version "7.1.2"
+  id("com.diffplug.spotless") version "6.19.0"
+  id("io.freefair.lombok") version "8.6"
+}
+
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(11))
+  }
+}
+
+group = "hu.bme.mit.ftsrg.chaincode.thesisportal"
+
+version = "0.1.0"
+
+repositories {
+  mavenCentral()
+  maven { url = uri("https://jitpack.io") }
+}
+
+dependencies {
+  implementation("org.hyperledger.fabric-chaincode-java:fabric-chaincode-shim:2.5.0")
+  implementation("com.google.code.gson:gson:2.10.1")
+  implementation("org.tinylog:tinylog-api:2.7.0")
+  implementation("org.tinylog:tinylog-impl:2.7.0")
+}
+
+application { mainClass.set("org.hyperledger.fabric.contract.ContractRouter") }
+
+tasks.named<ShadowJar>("shadowJar") {
+  archiveBaseName.set("chaincode")
+  archiveClassifier.set("")
+  archiveVersion.set("")
+}
+
+spotless {
+  java {
+    importOrder()
+    removeUnusedImports()
+    googleJavaFormat()
+    formatAnnotations()
+    licenseHeader("/* SPDX-License-Identifier: Apache-2.0 */")
+  }
+}
