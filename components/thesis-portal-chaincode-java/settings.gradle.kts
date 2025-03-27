d4b2a3f5316518d1eca36e5f5429c95146b9354a
bp99 <bertalan.peter@bp99.eu>
2025-03-27 12:23:20 +0100

    Fix application of foojay-resolver-convention plugin

--- components/thesis-portal-chaincode-java/settings.gradle.kts
+++ components/thesis-portal-chaincode-java/settings.gradle.kts
@@ -1,10 +1,5 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-    }
-    plugins {
-        id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-    }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "thesis-portal-chaincode"
