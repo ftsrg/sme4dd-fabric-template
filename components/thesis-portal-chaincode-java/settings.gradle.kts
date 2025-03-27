c20c2edaa3ad6f1b63a0d697b0c9c9b332963bfe
Attila Klenik <a.klenik@gmail.com>
2025-03-27 12:05:24 +0100

    Fixes
    Signed-off-by: Attila Klenik <a.klenik@gmail.com>

--- components/thesis-portal-chaincode-java/settings.gradle.kts
+++ components/thesis-portal-chaincode-java/settings.gradle.kts
@@ -1,1 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    }
+}
+
 rootProject.name = "thesis-portal-chaincode"
