718d13bbdf8b8cb703be26a11fc607f9e7f5996a
Mihály Dobos-Kovács <3751182+as3810t@users.noreply.github.com>
2025-07-01 14:50:04 +0000

    Remove spotless configuration

--- chaincode-java/build.gradle.kts
+++ chaincode-java/build.gradle.kts
@@ -5,7 +5,6 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
   application
   id("com.github.johnrengelman.shadow") version "7.1.2"
-  id("com.diffplug.spotless") version "6.19.0"
   id("io.freefair.lombok") version "8.6"
   id("org.sonarqube") version "6.2.0.5505"
 }
@@ -62,17 +61,6 @@ tasks.test {
 tasks.named<JavaExec>("run") {
   environment(
       mapOf("CORE_CHAINCODE_ID_NAME" to "hw-launch-codes", "CORE_PEER_ADDRESS" to "127.0.0.1:7041"))
-}
-
-spotless {
-  java {
-    importOrder()
-    removeUnusedImports()
-    googleJavaFormat()
-    formatAnnotations()
-    licenseHeader("/* SPDX-License-Identifier: Apache-2.0 */")
-  }
-  kotlinGradle { ktfmt() }
 }
 
 sonar {
